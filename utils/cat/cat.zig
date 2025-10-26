a1295a1f3b69c4ee784a7669e68db9f4c54fc278
Ali Aryani <lenesis@proton.me>
2025-10-27 01:48:18 +0330

    open file

--- utils/cat/cat.zig
+++ utils/cat/cat.zig
@@ -2,15 +2,26 @@ const std = @import("std");
 var stdout_buffer:[1024]u8 = undefined;
 var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
 const stdout = &stdout_writer.interface;
+const cwd = std.fs.cwd();
+
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer gpa.deinit();
     const allocator = gpa.allocator();
     const args = try std.process.argsAlloc(allocator);
     if (args.len < 2) {
         try stdout.print("Usage: cat <file>\n", .{});
-        try stdout.flush();
         return;
+    }
+
+    for (args) |filename| {
+        const file = try cwd.openFile(filename, .{.mode = .read_only});
+        defer file.close();
+
+
+
+
     }
 
 }
