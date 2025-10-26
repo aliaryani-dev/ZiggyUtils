796b07416c0f51ae1de23baa456ac3ed17b8107f
Ali Aryani <lenesis@proton.me>
2025-10-27 00:20:24 +0330

    get args

--- utils/cat/cat.zig
+++ utils/cat/cat.zig
@@ -1,12 +1,16 @@
 const std = @import("std");
-const args = std.process.args();
 var stdout_buffer:[1024]u8 = undefined;
 var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
 const stdout = &stdout_writer.interface;
 
 pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    const args = try std.process.argsAlloc(allocator);
     if (args.len < 2) {
         try stdout.print("Usage: cat <file>\n", .{});
+        try stdout.flush();
         return;
     }
+
 }
