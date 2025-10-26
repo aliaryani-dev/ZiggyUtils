c66fe829888f4e76f29b436350a2cceabc16c563
Ali Aryani <lenesis@proton.me>
2025-10-26 23:51:09 +0330

    check args

--- utils/cat/cat.zig
+++ utils/cat/cat.zig
@@ -5,4 +5,8 @@ var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
 const stdout = &stdout_writer.interface;
 
 pub fn main() !void {
+    if (args.len < 2) {
+        try stdout.print("Usage: cat <file>\n", .{});
+        return;
+    }
 }
