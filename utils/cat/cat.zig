4dff0ee81d68377ab7a542261fb96b9917012229
Ali Aryani <lenesis@proton.me>
2025-10-27 02:50:55 +0330

    print out file

--- utils/cat/cat.zig
+++ utils/cat/cat.zig
@@ -6,22 +6,25 @@ const cwd = std.fs.cwd();
 
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer gpa.deinit();
-    const allocator = gpa.allocator();
+    const allocator = std.heap.page_allocator;
     const args = try std.process.argsAlloc(allocator);
     if (args.len < 2) {
         try stdout.print("Usage: cat <file>\n", .{});
+        try stdout.flush();
         return;
     }
 
-    for (args) |filename| {
+    for (args[1..]) |filename| {
         const file = try cwd.openFile(filename, .{.mode = .read_only});
         defer file.close();
 
-
-
-
+        var buffer:[1024 * 1024]u8 = undefined;
+        while (true) {
+            const nread = try file.read(buffer[0..]);
+            if (nread <= 0) break;
+            try stdout.writeAll(buffer[0..nread]);
+            try stdout.flush();
+        }
     }
 
 }
