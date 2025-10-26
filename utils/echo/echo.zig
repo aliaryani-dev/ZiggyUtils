ef657b9d63919b053fe4fd4dccb0f0171678d40f
Ali Aryani <lenesis@proton.me>
2025-10-27 03:24:00 +0330

    basic echo functionality

--- utils/echo/echo.zig
+++ utils/echo/echo.zig
@@ -4,5 +4,17 @@ var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
 const stdout = &stdout_writer.interface;
 
 pub fn main() !void {
-
+    const allocator = std.heap.page_allocator;
+    const args = try std.process.argsAlloc(allocator);
+    if (args.len == 1) {
+        try stdout.print("Usage: echo <text>\n", .{});
+        try stdout.flush();
+        std.posix.exit(1);
+    }
+    
+    for (args[1..]) |text| {
+        try stdout.print("{s} ", .{text});
+    }
+    try stdout.print("\n", .{});
+    try stdout.flush();
 }
