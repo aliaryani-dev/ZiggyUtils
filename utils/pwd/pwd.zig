4c16cb1427f4de3b16d860dc33fb5c39bde392c2
Ali Aryani <lenesis@proton.me>
2025-10-27 22:53:05 +0330

    get PWD; print it out

--- utils/pwd/pwd.zig
+++ utils/pwd/pwd.zig
@@ -4,4 +4,8 @@ var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
 const stdout = &stdout_writer.interface;
 
 pub fn main() !void {
+    const path = std.posix.getenv("PWD");
+    if (path) |not_null_path|
+        try stdout.print("{s}\n", .{not_null_path});
+    try stdout.flush();
 }
