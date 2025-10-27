8f1ccea2e3adef67ae7cd0121509ee5703b2f50f
Ali Aryani <lenesis@proton.me>
2025-10-27 15:58:49 +0330

    get args; print usage

--- utils/tail/tail.zig
+++ utils/tail/tail.zig
@@ -4,4 +4,11 @@ var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
 const stdout = &stdout_writer.interface;
 
 pub fn main() !void {
+    const allocator = std.heap.page_allocator;
+    const args = try std.process.argsAlloc(allocator);
+    if(args.len == 1) {
+        try stdout.print("Usage: tail <file>\n", .{});
+        try stdout.flush();
+        std.posix.exit(1);
+    }
 }
