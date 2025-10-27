0613bb66806753f27778ee47a5365ef908c2320f
Ali Aryani <lenesis@proton.me>
2025-10-27 14:38:10 +0330

    implement -n

--- utils/head/head.zig
+++ utils/head/head.zig
@@ -8,16 +8,19 @@ pub fn main() !void {
     const allocator = std.heap.page_allocator;
     const args = try std.process.argsAlloc(allocator);
     if (args.len == 1) {
-        try stdout.print("Usage: head <file>\n", .{});
+        try stdout.print("Usage: head [-n <NUM>] <file>\n", .{});
         try stdout.flush();
         std.posix.exit(1);
     }
 
-    _ = try read_lines(args[1]);
-
+    if (std.mem.eql(u8, args[1], "-n")) {
+        _ = try read_lines(args[3],try std.fmt.parseInt(usize, args[2], 10));
+    } else {
+    _ = try read_lines(args[1],10);
+    }
 }
 
-fn read_lines(file_name:[]const u8) !void {
+fn read_lines(file_name:[]const u8,line_count:usize) !void {
     const file = try cwd.openFile(file_name, .{.mode = .read_only});
     defer file.close();
     var buffer:[1024]u8 = undefined;
@@ -25,7 +28,7 @@ fn read_lines(file_name:[]const u8) !void {
     var it = std.mem.splitScalar(u8, buffer[0..nread], '\n');
     var i:u32 = 0;
     while (it.next()) |line|{
-        if (i == 5) {
+        if (i == line_count) {
             break;
         } 
         try stdout.print("{d} | {s}\n", .{(i+1),line});
