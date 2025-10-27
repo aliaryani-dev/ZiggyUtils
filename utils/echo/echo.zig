a1161e0a4b64db56ee64aa4f6a47ff32ca77ab59
Ali Aryani <lenesis@proton.me>
2025-10-27 03:31:26 +0330

    check for -e

--- utils/echo/echo.zig
+++ utils/echo/echo.zig
@@ -12,11 +12,17 @@ pub fn main() !void {
         std.posix.exit(1);
     }
     var new_line:bool = true;
+    var escape_char = false;
     for (args[1..]) |text| {
         if(std.mem.eql(u8, text, "-n")) {
             new_line = false;
             continue;
         }
+        if(std.mem.eql(u8, text, "-e")) {
+            escape_char = true;
+            continue;
+        }
+
         try stdout.print("{s} ", .{text});
     }
     if(new_line)
