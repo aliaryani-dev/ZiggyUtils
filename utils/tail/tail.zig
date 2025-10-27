2719433934500eaa5c387735ee4db8e3bf5643c6
Ali Aryani <lenesis@proton.me>
2025-10-27 17:08:34 +0330

    print last 5 lines

--- utils/tail/tail.zig
+++ utils/tail/tail.zig
@@ -17,9 +17,26 @@ pub fn main() !void {
 }
 
 pub fn print_lines(filename:[]const u8) !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
     const file = try cwd.openFile(filename, .{.mode = .read_only});
     defer file.close();
     var buffer:[1024]u8 = undefined;
     const nread = try file.readAll(&buffer);
-    std.debug.print("{s}\n", .{buffer[0..nread]});
+    var it = std.mem.splitBackwardsScalar(u8, buffer[0..nread], '\n');
+    var list = try std.ArrayList([]u8).initCapacity(allocator, 100);
+    defer list.deinit(allocator);
+    var i:u32 = 0;
+    while (it.next()) |line| {
+        if (i==5) break;
+        try list.append(allocator, @constCast(line));
+        i += 1;
+    }
+    std.mem.reverse([]u8, list.items);
+    
+    var line_number:u32 = 1;
+    for (list.items) |line| {
+        try stdout.print("{d} | {s}\n", .{line_number, line});
+        line_number += 1;
+    } try stdout.flush();
 }
