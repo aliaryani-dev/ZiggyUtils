a69fdd5483c7fb60af6117d71dc880c65be7dca3
Ali Aryani <lenesis@proton.me>
2025-10-27 13:52:27 +0330

    get args; print usage

--- utils/head/head.zig
+++ utils/head/head.zig
@@ -2,6 +2,14 @@ const std = @import("std");
 var stdout_buffer:[1024]u8 = undefined;
 var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
 const stdout = &stdout_writer.interface;
+const cwd = std.fs.cwd();
 
 pub fn main() !void {
+    const allocator = std.heap.page_allocator;
+    const args = try std.process.argsAlloc(allocator);
+    if (args.len == 1) {
+        try stdout.print("Usage: head <file>\n", .{});
+        try stdout.flush();
+        std.posix.exit(1);
+    }
 }
